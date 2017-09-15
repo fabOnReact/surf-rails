1845da76b80808d4fc2b09c38a5853a9ede8683a
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-09-15 17:53:12 +0700

    fixing syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable
 	devise :database_authenticatable, :registerable,
-		:recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2
+		:recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 	def self.from_omniauth(access_token)
 	    data = access_token.info
 	    user = User.where(email: data['email']).first
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 	        user = User.create(name: data['name'],
 	           email: data['email'],
 	           password: Devise.friendly_token[0,20]
-	        )
+	           )
 	    end
 	    user
 	end
