3db9adffe0969bddd1eda066ac493bdf6b63e26e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-09-15 22:53:06 +0700

    setting in devise.rb the callback url

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,5 +275,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
-  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"]
+  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: "https://surfcheck.herokuapp.com/"
 end
