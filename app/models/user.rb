c03e2819a83fc187f80116eff1a345d3cbbbac7e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-09-15 21:30:09 +0700

    removing name in the omniauth authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
 	    # Uncomment the section below if you want users to be created if they don't exist
 	    unless user
-	        user = User.create(name: data['name'],
+	        user = User.create( #name: data['name'],
 	           email: data['email'],
 	           password: Devise.friendly_token[0,20]
 	           )
