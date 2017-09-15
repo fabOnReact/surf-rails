525ecbd06de058dab9a66e1b98efad45659f92d9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-09-15 20:52:39 +0700

    heroku update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	resources :pictures
+	resources :picturese
 	devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 	root 'pictures#new'
 end
