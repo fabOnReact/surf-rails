d4d045b032db52a7993eaddede74f30928370ea9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-09-15 17:50:53 +0700

    confiuring omniauth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://surfcheck.herokuapp.com/' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	resources :pictures
 	devise_for :users
+	devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 	root 'pictures#new'
 end
