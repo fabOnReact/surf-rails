d016a028885c1a50c19a3c68b1b4bf6cec211c4e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-12-04 13:32:24 +0100

    rspec test cases for Facebook Authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,6 +275,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
-  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: "https://surfcheck.herokuapp.com/users/auth/facebook/callback"
-  # config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: "http://127.0.0.1:3000/users/auth/facebook/callback"
+  # config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: "https://surfcheck.herokuapp.com/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: "http://127.0.0.1:3000/users/auth/facebook/callback"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each) { @user = FactoryGirl.build(:user)}
+  before(:each) { @user = FactoryBot.create(:user)}
 
   subject { @user }
 
@@ -10,4 +10,11 @@ RSpec.describe User, type: :model do
   it '#email returns a string' do
   	expect(@user.email.class).to be(String)
   end 
+
+  describe '.from_omniauth' do
+  	it 'should respond with the user' do
+	  	token = OmniAuth::AuthHash.new({"info" => {"email" => @user.email }}) 	
+	  	expect(User.from_omniauth(token)).to be_instance_of(User)
+  	end
+  end
 end
