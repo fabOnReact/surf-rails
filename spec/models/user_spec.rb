859bc5ca44c7dc1285832f5c3ff1616c60c04051
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-12-04 14:19:18 +0100

    rspec user model - test cases for rspec user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe User, type: :model do
 	  	expect(User.from_omniauth(token)).to be_instance_of(User)
   	end
   end
+
+  describe '.new_with_session' do
+  	it 'should set the email' do
+  		session = {"devise.facebook_data" => {"email" => "test@email.com", "extra" => {"raw_info" => {"email" => "second@email.com"}}}}
+  		params = {"password" => "12345678", "password_confirmation" => "12345678"}
+  		expect(User.new_with_session(params, session).email).to eql("second@email.com")
+  	end
+  end
 end
