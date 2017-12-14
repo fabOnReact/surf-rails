d1c2716767615a2e5738fd159e890e121f71ab67
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-12-14 16:16:15 +0100

    geocoder configuration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
-	validates :stars, numericality: { only_integer: true }
 	belongs_to :user
 	mount_uploader :picture, PictureUploader
+  geocoded_by :ip_address, :latitude => :lat, :longitude => :lon
+  after_validation :geocode
+
+  def ip_address
+    #if request.remote_ip == '127.0.0.1'
+      #'98.236.166.116'
+    #else
+      #request.remote_ip
+    #end
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214120434) do
+ActiveRecord::Schema.define(version: 20171214124705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20171214120434) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "picture"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
