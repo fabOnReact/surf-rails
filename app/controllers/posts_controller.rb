cb85e9b170ee86a1b96117bd7f45adc44881f61b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-12-14 16:23:47 +0100

    posts specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:description, :stars, :user_id)
+      params.require(:post).permit(:description, :user_id)
     end
 end
