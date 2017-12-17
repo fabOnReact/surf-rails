e945f01dccda097a1c60604272218f19e75e1159
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-12-17 21:05:01 +0100

    jquery geolocation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,26 @@
+$(document).on('turbolinks:load', function() {
+  
+  var location_path = $(location).attr('pathname');
+  if (location_path == '/posts/new') {
+    getLocation();
+  }  
+  /*getLocation();*/
+
+  function getLocation(){
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert('Geolocation is not supported by this browser.')
+    }
+  }
+
+  function showPosition(position) {
+    alert('Latitude: ' + position.coords.latitude + "Longitude: " + position.coords.longitude);
+  }
+
+});
+
+
 // js animation for scrolling down page with arrow button
 $('#circleArrow').hover(function(){
     $(this).effect("bounce", {times: 3}, 2000);
