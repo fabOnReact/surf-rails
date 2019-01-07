13f1aec47e60057fa0aa24e32ef64b9dd2fcbed0
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-01-07 16:14:19 +0100

    adding api login to registration controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  skip_before_action :verify_authenticity_token, only: [:create, :new]
+  # before_action :configure_sign_in_params, only: [:create]
+
+  def create
+    respond_to do |format|
+      format.json { 
+        self.resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+        render :status => 200, :json => resource
+      }
+      format.html { super }
+    end
+  end
+
+  def login_failed
+    render json: {error: 'Login failed, credentials invalid'}, status: 401
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-	resources :posts
-	get 'posts_pictures', to: 'posts#pictures', as: 'posts_pictures'
-	devise_for :users, path: '' #, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-	root 'posts#landing'
+  resources :posts
+  get 'posts_pictures', to: 'posts#pictures', as: 'posts_pictures'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  root 'posts#landing'
 end
