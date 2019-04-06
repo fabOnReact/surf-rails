7d7aeefec0b79f7615b848e515a63d3f288edb60
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-06 12:44:19 +0200

    adding date and location to response

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,20 @@ class Post < ApplicationRecord
   attr_accessor :ip_code
 
   mount_uploader :picture, PictureUploader
-  reverse_geocoded_by :latitude, :longitude, address: :location
+
+  # reverse_geocoded_by :latitude, :longitude, address: :location
+  reverse_geocoded_by :latitude, :longitude do |obj, results| 
+    if geo = results.first
+      obj.location = geo.city
+    end
+  end
+
+  # after_validation :fetch_address
   after_validation :reverse_geocode
+
+  def creation_date
+    created_at.strftime("%B %d, %Y, %A")
+  end
 
   def utc_date
     created_at.utc
--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! post, :id, :description, :created_at, :updated_at, :user_id, :picture, :latitude, :longitude, :location
+json.extract! post, :id, :description, :created_at, :updated_at, :user_id, :picture, :latitude, :longitude,:location
+json.date  post.creation_date
 json.url post_url(post, format: :json)
