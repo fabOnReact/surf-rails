d7cda2e017287e8831f17e2e35082f35e6d9ce62
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-08 15:50:41 +0200

    adding likes column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190406181429) do
+ActiveRecord::Schema.define(version: 20190408121508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20190406181429) do
     t.float "longitude"
     t.string "address"
     t.string "city"
+    t.integer "likes", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
