ee7a3d4b118f92824d3aaf93cc366e13dc0283a0
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-15 20:29:21 +0200

    location model handle scraped data

---
+++ app/models/location.rb
@@ -0,0 +1,10 @@
+class Location < ApplicationRecord
+  after_validation :reverse_geocode, if: ->(obj){ obj.latitude.present? and obj.longitude.present? }
+
+  reverse_geocoded_by :latitude, :longitude do |obj, results|
+    geo = results.first
+    if geo.data["error"].nil?
+      obj.address = geo.address
+    end
+  end
+end
