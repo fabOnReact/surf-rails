4fbe099a07f8acc2fda5c3ac7d8da95ee1773c7e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-05-04 20:15:10 +0200

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190416144857) do
+ActiveRecord::Schema.define(version: 20190504173719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20190416144857) do
     t.string "bottom"
     t.string "wave_quality"
     t.string "name"
-    t.string "latitude"
-    t.string "longitude"
+    t.float "latitude"
+    t.float "longitude"
     t.string "country"
     t.string "area"
     t.string "address"
