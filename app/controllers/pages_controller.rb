249c69a64bd44e9a98d098ee8a736dac76771076
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-05-14 23:00:29 +0200

    pages controller handling static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   acts_as_token_authentication_handler_for User, except: [:show]
   def show
+    params[:page] ||= "landing"
     render template: "pages/#{params[:page]}"
   end
 end
