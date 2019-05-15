bbce8645e0a4c478baddf66879a403de3338833c
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-05-15 18:48:50 +0200

    hide navbar for android requests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def mobileApp?
+    android? || iphone?
+  end
+  
+  def landingPage?
+    params[:page] == "landing"
+  end
+
+  def navbar?
+    mobileApp? || landingPage?
+  end
+
+  def android?
+   request.env["HTTP_USER_AGENT"].match(/Android/).present?
+  end
+
+  def iphone?
+    false
+  end
 end
