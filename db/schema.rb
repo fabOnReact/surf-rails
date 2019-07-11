67f34d3b6e49122f55e654cb392d9c7342481770
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-11 21:49:00 +0800

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190513142807) do
+ActiveRecord::Schema.define(version: 20190711115821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20190513142807) do
     t.string "city"
     t.integer "likes", default: 0
     t.integer "favorite", default: [], array: true
+    t.jsonb "forecast"
     t.index ["favorite"], name: "index_posts_on_favorite", using: :gin
     t.index ["latitude", "longitude"], name: "index_posts_on_latitude_and_longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
