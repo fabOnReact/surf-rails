b1e3b3a6b3dcc0c99c4e3f6e690ff4b0fd121ee1
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-19 15:49:32 +0800

    strong params core extension specs for corners?

--- spec/lib/strong_parameters_spec.rb
+++ spec/lib/strong_parameters_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 require 'core_ext/actionpack/lib/action_controller/metal/strong_parameters'
 
 describe ActionController::Parameters do
-  ActionController::Parameters.include(Parameters::Validations)
+  ActionController::Parameters.include(Parameters::Location)
   let(:empty_coordinates) { ActionController::Parameters.new({latitude: "", longitude: ""}) }
   let(:coordinates) { ActionController::Parameters.new({latitude: "123", longitude: "456"}) }
+  let(:pagination) { ActionController::Parameters.new({per_page: 2, page: 1}) }
+
+  let(:corners) do 
+    ActionController::Parameters.new(
+      { 
+        southWest: { latitude: 10, longitude: 5 },
+        northEast: { latitude: 5, longitude: 15 }
+      } 
+    )
+  end
+
   describe '#with_location?' do
     it 'check that gps coordinates are present' do
       expect(empty_coordinates.location?).to be false
@@ -15,6 +26,16 @@ describe ActionController::Parameters do
   describe '#gps' do 
     it 'returns an array with latitude and longitude' do
       expect(coordinates.gps).to eql ['123', '456']
+    end
+  end
+
+  describe '#corners?' do
+    it 'returns true if params are present' do 
+      expect(corners.corners?).to be true
+    end
+
+    it 'returns true if params are present' do 
+      expect(coordinates.corners?).to be false
     end
   end
 end
