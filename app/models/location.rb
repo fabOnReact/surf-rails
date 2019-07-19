69d97f8b420bd1b5ea2574ddcffb60471f1f31c2
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-19 15:45:12 +0800

    location model callback to set forecast and job

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
-require 'api/storm_glass'
+require 'api/storm_glass' 
 
 class Location < ApplicationRecord
+  before_save :set_forecast
   after_validation :reverse_geocode, if: ->(obj){ obj.latitude.present? and obj.longitude.present? }
   has_many :posts
 
@@ -9,6 +10,15 @@ class Location < ApplicationRecord
     if geo && geo.data["error"].nil?
       obj.address = geo.address
     end
+  end
+
+  def set_forecast
+    set_job if forecast.nil?
+    self.forecast = api.getWaveForecast
+  end
+
+  def set_job
+    Sidekiq::Cron::Job.create(name: 'Location - update forecast data - every 24 hours', cron: '* 24 * *', class: 'LocationWorker', args: self.id ) 
   end
 
   def current_forecast
@@ -23,4 +33,3 @@ class Location < ApplicationRecord
     DateTime.now.utc.in_time_zone(-1).beginning_of_hour.xmlschema
   end
 end
-
