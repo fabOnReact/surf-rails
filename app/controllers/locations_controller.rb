d78c11c6294ae15cf4990c8aa4908d3caf317193
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-19 16:39:48 +0800

    fixing issues with locations queries

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,9 +6,11 @@ class LocationsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @locations = Location.near(params.gps, 20, units: :km)
-    @locations = Location.within_bounding_box(params.corners).paginate(page: params[:page], per_page: params[:per_page]) if params.corners?
-    @locations = @locations.paginate(page: params[:page], per_page: params[:per_page])
-    @locations.where(forecast: nil).each {|location| location.save }
+    if params.corners?
+      @locations = Location.within_bounding_box(params.corners) 
+    else
+      @locations = Location.near(params.gps, 20, units: :km).paginate(page: params[:page], per_page: params[:per_page]) 
+      @locations.where(forecast: nil).each {|location| location.save }
+    end
   end
 end
