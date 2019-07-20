9fd9b58d038c144fb80950b997c05a15bfc15407
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-20 21:10:18 +0800

    minor change to spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,4 +3,9 @@ require 'api/storm_glass'
 
 RSpec.describe Location, type: :model do
   let(:location) { FactoryBot.create(:location) }
+
+  describe "#set_forecast" do 
+    it "triggers and records a job" do
+    end
+  end
 end
