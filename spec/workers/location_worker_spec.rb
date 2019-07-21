46d180ac6b9368a93de633159ee214730f99d6d9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-21 12:54:47 +0800

    location worker specs

--- spec/workers/location_worker_spec.rb
+++ spec/workers/location_worker_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 RSpec.describe LocationWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:location) { FactoryBot.create(:location, forecast: nil, latitude: 1, longitude: 1) } 
+  describe '#perform' do
+    it 'should update the forecast attribute of location' do
+      LocationWorker.new.perform(location.id)
+      expect(location.reload.forecast).not_to be nil
+    end
+
+    it 'should trigger runtime error if api fails' do
+      allow_any_instance_of(StormGlass).to receive(:weather).and_return({errors: 'some error'})
+      expect{Location.new.perform(location.id)}.to raise_error(RuntimeError)
+    end
+  end
 end
