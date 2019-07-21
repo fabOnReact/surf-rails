c12967e8839e03bf41f1669d5984b3fc377383fb
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-21 12:52:23 +0800

    separating save logic from the LocationWorker

--- app/workers/location_worker.rb
+++ app/workers/location_worker.rb
@@ -1,7 +1,14 @@
+require 'api/storm_glass' 
+
 class LocationWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    Location.find(args.first).save
+    @location = Location.find(args.first)
+    @location.update_attribute(:forecast, api.getWaveForecast)
+  end
+
+  def api 
+    @api = StormGlass.new(@location.latitude, @location.longitude)
   end
 end
