0c2da94debef780c0b2f137fca486df497ca246e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-29 10:24:24 +0800

    renaming tideChart.js

---
+++ app/javascript/lib/tideChart.js
@@ -0,0 +1,61 @@
+import Chart from 'chart.js';
+
+export function renderTideChart(data, times) {
+  var ctx = document.getElementById('tideChart');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: times,
+        datasets: [{
+          data: data,
+          backgroundColor: ["rgb(0, 128, 0)"],
+          borderColor: [],
+          borderWidth: 1,
+          pointRadius: 0,
+        }]
+    },
+    options: {
+      legend: { display: false },
+      responsive: true,
+      mantainAspectRatio: false,
+      scales: {
+        yAxes: [{
+            scaleLabel: { display: false },
+            weight: 0,
+            ticks: {
+              beginAtZero: true,
+              display: false,
+            },
+            gridLines: { 
+                display: true,
+                color: 'rgb(204, 0, 0)',
+                lineWidth: 0,
+            },
+            tickMarkLength: 0,
+        }],
+        xAxes: [{
+          display: true,
+          weight: 0,
+          gridLines: {
+            display: true,
+            zeroLineWidth: 0,
+            zeroLineColor: '#ffcc33',
+            tickMarkLength: 10,
+          },
+          ticks: {
+            display: true,
+            fontColor: 'black',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            fontSize: 10,
+            lineHeight: 1,
+            maxRotation: 0,
+            minRotation: 0,
+            autoSkip:false,
+          },
+          color: 'red',
+        }],
+      }
+    }
+  });
+}
