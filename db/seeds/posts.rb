e01100e0b4db99f9a184053920733d5741706f2e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-13 11:26:37 +0800

    seed files

---
+++ db/seeds/posts.rb
@@ -0,0 +1,38 @@
+balangan = ['balangan.jpg','balangan_0728.JPG','balangan_0731.JPG','balangan_0732.JPG','balangan_0733.JPG','balangan_0735.JPG','balangan_0736.JPG','balangan_0737.JPG','balangan_0739.JPG','balangan_0740.JPG']
+bondi = ['bondi_0466.JPG','bondi_0467.JPG','bondi_0468.JPG','bondi_0471.JPG','bondi_0472.JPG','bondi_0473.JPG','bondi_0484.JPG','bondi_0494.JPG','bondi_0503.JPG','bondi_0504.JPG','bondi_0505.JPG','bondi_0506.JPG','bondi_0507.JPG','bondi_0511.JPG','bondi_0512.JPG','bondi_0513.JPG','bondi_0523.JPG','bondi_0554.JPG','bondi_0567.JPG','bondi_0568.JPG','bondi_0569.JPG','bondi_0570.JPG','bondi_0571.JPG','bondi_0572.JPG','bondi_0573.JPG','bondi_0596.JPG']
+         
+kuta = ['kuta_0687.JPG']
+
+domain = 'https://surfcheck.s3.eu-central-1.amazonaws.com'
+
+user = User.new(email: "admin@user.com", password: "fabrizio")
+
+balangan.each do |picture_name|
+  image_url = "#{domain}/#{picture_name}"
+  location = Location.find_by(name: "Balangan")
+  post = Post.new(user: user, longitude: location.longitude, latitude: location.latitude)
+  post.remote_picture_url = image_url
+  post.save
+  puts "post saved, picture url: #{post.picture.url}" if post.valid?
+  puts post.errors.full_messages unless post.valid?
+end
+
+bondi.each do |picture_name|
+  image_url = "#{domain}/#{picture_name}"
+  location = Location.find_by(name: "Bondi Beach")
+  post = Post.new(user: user, longitude: location.longitude, latitude: location.latitude)
+  post.remote_picture_url = image_url
+  post.save
+  puts "post saved, picture url: #{post.picture.url}" if post.valid?
+  puts post.errors.full_messages unless post.valid?
+end
+
+kuta.each do |picture_name|
+  image_url = "#{domain}/#{picture_name}"
+  location = Location.find_by(name: "Kuta Beach")
+  post = Post.new(user: user, longitude: location.longitude, latitude: location.latitude)
+  post.remote_picture_url = image_url
+  post.save
+  puts "post saved, picture url: #{post.picture.url}" if post.valid?
+  puts post.errors.full_messages unless post.valid?
+end
