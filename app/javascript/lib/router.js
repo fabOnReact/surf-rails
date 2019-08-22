e044a249ac706f9f0ccae1ad61ee5c3dbc0a0099
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-22 15:19:47 +0800

    disabling link /post js logic

--- app/javascript/lib/router.js
+++ app/javascript/lib/router.js
@@ -8,7 +8,7 @@ export function router() {
   switch(location_path) {
     case '/':
         // containerStyle();
-        setLocation()
+        // setLocation()
         break;
     case '/posts':
     case '/users/sign_in':
