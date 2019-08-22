54d978a025fde8557ad5d7099a2cb6cf1c8488cf
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-22 23:26:15 +0800

    deleting old serializers

--- app/serializers/posts/index_serializer.rb
+++
@@ -1,5 +0,0 @@
-class Posts::IndexSerializer < PostSerializer
-  def forecast
-    { tideChart: object.location.forecast.tideChart, hourly: object.location.hourly, daily: object.location.daily  } if object.location.forecast.current
-  end
-end
