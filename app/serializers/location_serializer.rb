124b3c462ef3cfd339947e09ef38ab7444078897
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-23 23:50:40 +0800

    renaming serializer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,4 +1,4 @@
-class LocationsSerializer
+class LocationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :direction, :experience, :frequency, :bottom, :wave_quality, :name, :latitude, :longitude, :country, :area, :address, :longitude, :latitude
 
@@ -7,6 +7,6 @@ class LocationsSerializer
   end
 
   attribute :distance do |object|
-    object.distance_from_user(params.gps) if params.gps?
+    @distance
   end
 end
