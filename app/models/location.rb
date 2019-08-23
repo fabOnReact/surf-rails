4f99dc3ce84909e899e0654b0a4b73a26f34028a
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-23 23:03:13 +0800

    moving logic from model to background job

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,14 +1,14 @@
 require 'api/storm' 
-require 'api/google'
 require 'core_ext/string'
 require 'core_ext/hash'
+require 'api/google'
 require 'forecast'
 
 class Location < ApplicationRecord
   String.include(String::Weather)
   Hash.include(Hash::Weather)
 
-  before_save :set_forecast, if: Proc.new {|location| location.forecast.eql? [] }
+  # before_save :set_forecast, if: Proc.new {|location| location.forecast.eql? [] }
   after_validation :reverse_geocode, if: ->(obj){ valid_coordinates(obj) }
   has_many :posts
   has_many :forecasts
@@ -41,37 +41,23 @@ class Location < ApplicationRecord
 
   def gps; [latitude, longitude]; end
 
-  def set_forecast
-    set_job
-    self.tide = storm.getTide
-    self.forecast = storm.getWaveForecast
-    self.timezone = maps.getTimezone
-    self.daily = forecast.daily("waveHeight", timezone)
-    self.hourly = forecast.hourly if forecast.current
-  end
-
   def offsetHours
     timezone["rawOffset"] / 3600
   end
 
   def set_job
-    Sidekiq::Cron::Job.load_from_array(jobs_params) unless Rails.env.test?
-  end
-
-  def maps
-    @maps = Google::Maps.new(gps.join(','))
   end
 
   def storm
     @storm = Storm.new(latitude, longitude)
   end
 
+  def maps
+    @maps = Google::Maps.new(gps.join(','))
+  end
+
   def google_map
     gpsString = gps.join(',')
     "https://maps.googleapis.com/maps/api/staticmap?center=#{gpsString}&zoom=11&markers=#{gpsString}&key=#{ENV['GOOGLE_MAPS_API_KEY']}&size=300x300&maptype=satellite"
-  end
-
-  def jobs_params
-    [{ name: "Location name: #{self.name}, country: #{self.country}, id: #{self.id} - update forecast data - every day at 00:00", cron: "0 0 * * *", class: 'LocationWorker', args: {id: self.id, hourly: false}}, { name: "Location name: #{self.name}, country: #{self.country}, id: #{self.id} - update hourly forecast data - every hour at 00:00", cron: "0 * * * *", class: 'LocationWorker', args: { id: self.id, hourly: true}}]
   end
 end
