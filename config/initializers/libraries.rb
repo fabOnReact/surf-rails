5edccdaee8b9c00399f9fe5c15677b8ed4dac945
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-23 10:54:07 +0800

    renaming and requiring libraries in initializer

--- config/initializers/libraries.rb
+++ config/initializers/libraries.rb
@@ -3,3 +3,10 @@ Dir[File.join(Rails.root, 'lib', 'core_ext','*.rb')].each do |file|
 end
 
 Dir[File.join(Rails.root, 'lib', 'core_ext', 'action_dispatch', 'http','*.rb')].each {|l| require l}
+
+require 'mechanize'
+require 'scraper'
+require 'continent'
+require 'scraper/spot'
+require 'scraper/country'
+
