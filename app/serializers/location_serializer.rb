1fc51f975ab578592b2657cc83df369bb8176072
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-24 15:48:09 +0800

    moving logic to location serializer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,12 +1,13 @@
 class LocationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :direction, :experience, :frequency, :bottom, :wave_quality, :name, :latitude, :longitude, :country, :area, :address, :longitude, :latitude
+  attributes :id, :direction, :experience, :frequency, :bottom, :wave_quality, :name, :latitude, :longitude, :country, :area, :address, :longitude, :latitude, :with_forecast
 
-  attribute :forecast do |object|
-    { tide: object.forecast.tideChart, hourly: object.hourly, daily: object.daily } if object.forecast.current
+  attribute :forecast_info do |object|
+    { tide: object.tide_chart, hourly: object.hourly, daily: object.daily } if object.with_forecast
   end
 
-  attribute :distance do |object|
-    @distance
+  
+  attribute :distance do |object, params|
+    object.distance_from_user(params[:gps]) if params[:gps].present?
   end
 end
