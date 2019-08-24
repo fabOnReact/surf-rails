6a6017fea979292fd4ce2ef44454777dfc429589
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-24 16:33:35 +0800

    v1 and v0 are merged

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,2 @@
 class Api::V1::PostsController < PostsController
-  before_action :decorate_posts, only: [:index]
-  
-  private
-  def decorate_posts
-    set_posts
-    @posts = @posts.map do |post| 
-      PostSerializer.new(post).serializable_hash[:data][:attributes]
-    end
-  end
 end
