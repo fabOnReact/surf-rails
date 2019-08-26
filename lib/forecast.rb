4c208e55d0b243e3eee2af03a178b618757bf797
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-26 17:21:25 +0800

    splitting forecast logic within two libraries

--- lib/forecast.rb
+++ lib/forecast.rb
@@ -3,16 +3,6 @@ require 'core_ext/array'
 class Forecast < Array
   Array.include(Array::Weather)
   Hash.include(Hash::Weather)
-  KEYS = %w(time swellHeight waveHeight windSpeed windDirection waveDirection swellDirection swellPeriod)
-
-  def tideChart
-    { hours: hours, seaLevels: seaLevels }
-  end
-
-  def upcomingWaves
-    upcoming.collectValues('waveHeight') {|x| x.values.average }[0..152]
-  end
-
   def current
     @current ||= select {|row| row["time"] == timeNow }.first
   end
@@ -21,90 +11,30 @@ class Forecast < Array
     DateTime.now.utc.in_time_zone(-1).beginning_of_hour.xmlschema
   end
 
-  def waveHeightTotal
-    total = forecast.sum {|h| h.value("waveHeight") }
-  end
-
-  def seaLevels
-    upcoming.map {|x| x.value("seaLevel") }[0..24]
-  end
-
-  def hours
-    upcoming.map {|x| x["time"] }[0..24]
-  end
-
   def days
     upcoming.map {|x| x["time"] }[0..152]
   end
 
   def upcoming
-    Forecast.new(select { |row| row["time"] >= timeNow })
+    select { |row| row["time"] >= timeNow }
+  end
+
+  def select
+    Forecast.new(super)
   end
 
   def values
     collect {|x| x["value"] }
   end
 
-  def hourlyAverage(key, day)
-    within(day).collectValues(key) do |x| 
-      x.values.average 
-    end
+  def time; 
+    current["time"]; 
   end
-
-  def dailyAverage(key, day)
-    hourlyAverage(key, day).average
-  end
-
-  def within(day)
-    Forecast.new(select do |row| 
-      datetime = row["time"].to_datetime
-      dayBegin = day.beginning_of_day + 6.hours
-      dayEnd = day.end_of_day - 6.hours
-      (dayBegin..dayEnd).cover? datetime
-    end)
-  end
-
-  def daily(key, timezone)
-    days = (DateTime.now..DateTime.now+6).map {|day| day.in_time_zone(timezone["timeZoneId"]) }
-    forecast = days.map {|day| dailyAverage(key, day)}.delete_if {|x| x.nil? } 
-    days = days.map {|x| x.to_datetime.strftime("%A") }[0..forecast.size-1]
-    { key => forecast, "days" => days }
-  end
-
-  def waveAverage; waveHeights.average; end
-
-  def periodsAverage; swellPeriods.average; end
 
   def collectValues(key)
     collect do |x| 
       row = Forecast.new(x[key])
       if block_given?; yield(row); else; row; end
     end
-  end
-
-  %w(swellHeight waveHeight windSpeed swellPeriod).each do |method|
-    define_method("#{method}Range") { current[method].minMaxString }
-  end
-
-  Forecast::KEYS.each do |method|
-    define_method(method) { current.value(method) } 
-  end
-
-  Forecast::KEYS.each do |method|
-    define_method("#{method}_at(time)".to_sym) do 
-      select {|row| row["time"] == time }.first.value(method) 
-    end
-  end
-
-  def hourly
-    Forecast::KEYS.map {|key| [key, send(key.to_sym)] }.to_h
-  end
-
-  def time; 
-    current["time"]; 
-  end
-
-  %w(waveHeight swellPeriod).each do |method|
-    define_method(method.pluralize) { current[method].collect {|x| x["value"]}}
   end
 end
---
+++ lib/forecast/wave.rb
@@ -0,0 +1,78 @@
+require 'forecast'
+
+class Forecast::Wave < Forecast
+  KEYS = %w(time swellHeight waveHeight windSpeed windDirection waveDirection swellDirection swellPeriod)
+
+  %w(swellHeight waveHeight windSpeed swellPeriod).each do |method|
+    define_method("#{method}Range") { current[method].minMaxString }
+  end
+
+  %w(waveHeight swellPeriod).each do |method|
+    define_method(method.pluralize) { current[method].collect {|x| x["value"]}}
+  end
+
+  Wave::KEYS.each do |method|
+    define_method(method) { current.value(method) } 
+  end
+
+  Wave::KEYS.each do |method|
+    define_method("#{method}_at(time)".to_sym) do 
+      select {|row| row["time"] == time }.first.value(method) 
+    end
+  end
+
+  def hourly
+    Wave::KEYS.map {|key| [key, send(key.to_sym)] }.to_h
+  end
+
+
+  def within(day)
+    Wave.new(select do |row| 
+      datetime = row["time"].to_datetime
+      dayBegin = day.beginning_of_day + 6.hours
+      dayEnd = day.end_of_day - 6.hours
+      (dayBegin..dayEnd).cover? datetime
+    end)
+  end
+
+  def daily(key, timezone)
+    days = (DateTime.now..DateTime.now+6).map {|day| day.in_time_zone(timezone["timeZoneId"]) }
+    forecast = days.map {|day| dailyAverage(key, day)}.delete_if {|x| x.nil? } 
+    days = days.map {|x| x.to_datetime.strftime("%A") }[0..forecast.size-1]
+    { key => forecast, "days" => days }
+  end
+
+  def hourlyAverage(key, day)
+    within(day).collectValues(key) do |x| 
+      x.values.average 
+    end
+  end
+
+  def dailyAverage(key, day)
+    hourlyAverage(key, day).average
+  end
+
+  def upcomingWaves
+    upcoming.collectValues('waveHeight') {|x| x.values.average }[0..152]
+  end
+
+  def waveHeightTotal
+    total = forecast.sum {|h| h.value("waveHeight") }
+  end
+
+  def waveAverage; waveHeights.average; end
+
+  def periodsAverage; swellPeriods.average; end
+
+  def tide
+    { hours: hours, seaLevels: seaLevels }
+  end
+
+  def seaLevels
+    upcoming.map {|x| x.value("seaLevel") }[0..24]
+  end
+
+  def hours
+    upcoming.map {|x| x["time"] }[0..24]
+  end
+end
