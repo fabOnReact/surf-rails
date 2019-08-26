533ef092dfa51253d4faaacf9a3cfbdc28518759
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-26 17:21:39 +0800

    background jobs to update forecasts

--- spec/workers/daily_forecast_worker_spec.rb
+++ spec/workers/daily_forecast_worker_spec.rb
@@ -1,37 +1,48 @@
 require 'rails_helper'
 RSpec.describe DailyForecastWorker, type: :worker do
-  let(:location) { FactoryBot.create(:location, latitude: -8.716364, longitude:115.166931 ) } 
-  let(:hour) do 
-    { 
-      "swellHeight"=> [{ "value" => 1}, { "value" => 2 }], 
-      "waveHeight"=> [{ "value" => 1}, { "value" => 2 }], 
-      "seaLevel"=>[{"value"=>0.27, "source"=>"sg"}],
-    }
+  subject { DailyForecastWorker.new }
+  let(:location) { instance_double("location") }
+  let(:forecast) { instance_double("forecast") }
+  let(:tide) { instance_double("tide") }
+  before do 
+    allow(Location).to receive(:find_by)
+      .with({ id: 1, with_forecast: false })
+      .and_return location 
   end
-  let(:date1) { instance_double("date") } 
-  let(:date2) { instance_double("date") } 
-  let(:forecast) do 
-    hour1, hour2 = hour
-    hour1["time"] = date1 
-    hour2["time"] = date2 
-    Forecast.new([ hour1, hour2 ])
-  end 
 
-  describe '#perform' do
-    it 'should update the forecast attribute of location' do
-      LocationWorker.new.perform({ "ids" => location.id })
-      expect(location.reload.forecast).not_to be nil
+  describe '#data_available?' do
+    it 'executes api call if current forecast is not available' do
+      allow(location).to receive(:current_forecast?).and_return(false)
+      expect(location).to receive_message_chain(:storm, :success?) { true }
+      subject.send(:set_location, { "id" => 1 })
+      expect(subject.data_available?).to be true
     end
 
-    it 'should upate the other columns' do 
-      LocationWorker.new.perform({ "ids" => location.id })
-      expect(location.reload.daily).not_to be nil
-      expect(location.reload.hourly).not_to be nil
+    it 'does not execute an api call if current forecast is available' do
+      allow(location).to receive(:current_forecast?).and_return(true)
+      expect(location).to_not receive(:storm)
+      subject.send(:set_location, { "id" => 1 })
+      expect(subject.data_available?).to be true
     end
+  end
 
-    it 'should trigger runtime error if api fails' do
-      allow_any_instance_of(Storm).to receive(:weather).and_return({'errors'=> 'some error'})
-      expect{LocationWorker.new.perform(location.id)}.to raise_error(TypeErrorr)
+  describe '#execute_job' do
+    context 'current forecast available in db' do
+      before do 
+        allow(subject).to receive(:forecast?) { true }
+        allow(location).to receive_message_chain(:forecast, :current) { forecast }
+        allow(subject).to receive(:timezone?) { false }
+        allow(subject).to receive(:set_timezone)
+        subject.send(:set_location, { "id" => 1 })
+      end
+      
+      it 'will update the with_forecast column' do
+        expect(subject).to receive(:get_forecast)
+        expect(subject).to receive(:update_data)
+        expect(location).to receive(:update).with(with_forecast: true)
+        subject.execute_job
+      end
     end
   end
 end
+
--- spec/workers/hourly_forecast_worker_spec.rb
+++ spec/workers/hourly_forecast_worker_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 RSpec.describe HourlyForecastWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { HourlyForecastWorker.new }
+  let(:location) { instance_double("location") }
+
+  before do 
+    allow(Location).to receive(:find_by)
+      .with({ id: 1 })
+      .and_return location 
+  end
+
+  describe '#perform' do 
+    it '#set_location if #valid_parameters' do
+    end
+  end
+
+  describe '#set_location' do
+    it 'returns the location instance' do
+      expect(subject.send(:set_location, { "id" => 1 })).to be location 
+    end
+  end
 end
