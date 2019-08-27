ecef46e7fe95299bca9a12a408aec01f012d9b38
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-27 23:07:45 +0800

    removing pages controller from v1

--- app/controllers/api/v1/pages_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::PagesController < PagesController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
       devise_scope :user do
         post "/users/sign_in", to: "users/sessions#create"
         post "/users", to: "users/registrations#create"
-        get "/pages/:page" => "pages#show", as: :page
       end
     end
   end
