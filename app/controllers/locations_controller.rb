df5d7ccb294913e303bd01eb435b01d73e7e02b4
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-05 17:42:05 +0800

    query up to 1000km if no results are given

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -20,6 +20,7 @@ class LocationsController < ApplicationController
   private
   def set_locations
     @locations = Location.near(params.gps, 70, units: :km)
+    @locations = Location.near(params.gps, 1000, units: :km) if @locations.empty?
     to_update = @locations.where(with_forecast: false).limit(8)
     @locations = @locations.where(with_forecast: true).limit(8)
     to_update.each {|location| location.set_job } if to_update.present?
