18485c509f13a5182510adbf60e6a574d774e250
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-08 16:39:34 +0800

    deleting old files and migr to forecast model

--- db/seeds/forecast.rb
+++
@@ -1,4 +0,0 @@
-locations = Location.where(forecast: nil)
-locations.update_all(with_forecast: false)
-locations = Location.where.not(forecast: nil)
-locations.update_all(with_forecast: true)
--- spec/lib/forecast/wave_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-require 'forecast'
-require 'forecast/wave'
-
-describe Forecast::Wave do
-end
