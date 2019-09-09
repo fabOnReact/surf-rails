fb4f87be5a2c554b4650aa2382f4d73a487fb269
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-09 23:58:52 +0800

    require libs

--- config/initializers/libraries.rb
+++ config/initializers/libraries.rb
@@ -2,10 +2,19 @@ Dir[File.join(Rails.root, 'lib', 'core_ext', '*.rb')].each do |file|
   require file
 end
 
-Dir[File.join(Rails.root, 'lib', 'core_ext', 'action_dispatch', 'http', '*.rb')].each { |l| require l }
+Dir[File.join(
+  Rails.root, 
+  'lib', 
+  'core_ext', 
+  'action_dispatch', 
+  'http', 
+  '*.rb'
+  )
+].each { |l| require l }
 
 require 'core_ext/string'
 require 'core_ext/hash'
+require 'core_ext/float'
 require 'mechanize'
 require 'scraper'
 require 'continent'
@@ -13,4 +22,4 @@ require 'scraper/spot'
 require 'scraper/country'
 require 'api/storm'
 require 'api/google'
-require 'forecast/weather'
+require 'weather'
