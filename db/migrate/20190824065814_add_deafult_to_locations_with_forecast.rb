45688a53eeb11d078d542633d9e201f9c457ccb2
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-10 17:02:31 +0800

    fixing migrations

--- db/migrate/20190824065814_add_deafult_to_locations_with_forecast.rb
+++ db/migrate/20190824065814_add_deafult_to_locations_with_forecast.rb
@@ -1,5 +1,8 @@
 class AddDeafultToLocationsWithForecast < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column :locations, :with_forecast, :boolean, default: false
+  end
+  def down
+    change_column :locations, :with_forecast, :boolean, default: nil
   end
 end
