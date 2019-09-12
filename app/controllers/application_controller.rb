92802f7b896cd2450834d87acea665ca570f3f5e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-12 22:45:45 +0800

    enable verify auth token for web

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   acts_as_token_authentication_handler_for User #, except: [:landing, :index]
-  protect_from_forgery with: :exception
+  protect_from_forgery unless: -> { request.format.json? }
+  skip_before_action :verify_authenticity_token, if: :json_request?
   # before_action :authenticate_user! #, except: [:landing, :index]
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def json_request?
+    request.format.json?
+  end
 
   def after_sign_in_path_for(resource) 
     posts_path(latitude: params[:latitude], longitude: params[:longitude]) 
