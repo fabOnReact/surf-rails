c92ea471f5e79bbe6420e3466532303edb98be78
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-12 22:47:07 +0800

    seed file to handle migration to forecast model

--- db/seeds/weather_migration.rb
+++ db/seeds/weather_migration.rb
@@ -1,17 +1,4 @@
-# locations = Location.where(forecast: nil)
-# locations.update_all(with_forecast: false)
-# locations = Location.where.not(forecast: nil)
-# locations.update_all(with_forecast: true)
-
-locations = Location.where(with_forecast: true)
-
-locations.each do |location|
-  Forecast.create(
-    location_id: location.id, 
-    weather: location.forecast_data,
-    tides: location.tides,
-    daily: location.forecast_daily,
-    hourly: location.forecast_hourly,
-    tide: location.forecast_tide
-  )
-end
+Forecast.destroy_all
+Location.update_all(with_forecast: false)
+Sidekiq::Cron::Job.destroy_all!
+Post.all.each {|post| post.location.set_job }
