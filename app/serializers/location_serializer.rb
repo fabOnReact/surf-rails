2257aee6d90001ff3ed4a18b5320a5a4819ee81c
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-13 00:21:03 +0800

    fixing location serializer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,8 +1,8 @@
 class LocationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :direction, :experience, :frequency, :bottom, :wave_quality, 
-    :name, :latitude, :longitude, :country, :area, :address,
-    :with_forecast, :best_wind_direction, :best_swell_direction, :forecast_info
+    :name, :latitude, :longitude, :address, :with_forecast, :best_wind_direction, 
+    :best_swell_direction, :forecast_info
 
   attribute :forecast_info do |object|
     forecast = object.forecast
