0ee17db42d5e595774138d079b4f37aa3d71976d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-12 22:46:33 +0800

    enable verify_auth_token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   acts_as_token_authentication_handler_for User, except: [:show]
+  skip_before_action :verify_authenticity_token
+
   def show
     params[:page] ||= "landing"
     render template: "pages/#{params[:page]}"
