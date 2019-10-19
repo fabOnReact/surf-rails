b94a70bff56436eb677bf4ed64bf6e09555cdfe9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-19 15:40:36 +0800

    removing webapp routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
-  resources :posts, :locations
+  # resources :posts, :locations
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
