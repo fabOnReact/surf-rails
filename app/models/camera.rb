9d80a7c688a0c30df3545bc1cb1793e8874ae903
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-19 17:37:32 +0800

    logic to create cameras and locations

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,2 +1,24 @@
 class Camera < ApplicationRecord
+  searchkick locations: [:location]
+  belongs_to :location
+  has_many :posts
+  before_validation :set_location
+  after_create :update_forecast
+  validates_associated :location, :message => "Looks like you are very far from any surf destination, only videos that are taken at a surfspot present in our database are accepted. Sorry!"
+
+  def search_data
+    attributes.merge(location: {lat: latitude, lon: longitude})
+  end
+
+  def coords
+    [latitude, longitude]
+  end
+
+  def set_location
+    self.location = Location.near(coords, 30).limit(1).first
+  end
+
+  def update_forecast
+    location.set_job unless location.with_forecast
+  end
 end
