c1cb512ce80a79f844e5d39da1fc7d618b4082da
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-31 10:25:52 +0800

    initialize or set camera

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
   include ActionView::Helpers::DateHelper
+  default_scope -> { where(flagged:false) }
   scope :newest, -> { order(created_at: :desc) }
   belongs_to :user 
   belongs_to :camera, touch: :last_post_at
-  before_validation :set_camera
+  before_validation :set_or_initialize_camera, :set_forecast
   after_validation :reverse_geocode
-  before_destroy :delete_orphaned_camera
+  after_destroy :delete_orphaned_camera
   attr_accessor :ip_code
 
   mount_uploader :picture, PictureUploader
@@ -16,28 +17,6 @@ class Post < ApplicationRecord
       obj.address = geo.address
       obj.city = geo.city if geo.city
     end
-  end
-
-  def geo_query 
-    { 
-      location: 
-        { 
-          near: 
-            { 
-              lat: latitude, 
-              lon: longitude
-            }, 
-          within: "1km"
-        }
-    }
-  end
-
-  def set_camera
-    self.camera = Camera.search(where: geo_query, limit: 1).first
-    self.camera = Camera.new(
-      latitude: latitude, 
-      longitude: longitude
-    ) unless camera
   end
 
   def liked(user_id)
@@ -57,11 +36,51 @@ class Post < ApplicationRecord
   end
 
   private
+  def geo_query 
+    { 
+      location: 
+        { 
+          near: 
+            { 
+              lat: latitude, 
+              lon: longitude
+            }, 
+          within: "1km"
+        }
+    }
+  end
+
+  def set_forecast
+    self.forecast = camera.location.forecast_hourly
+  end
+
+  def set_camera
+    self.camera = Camera.search(where: geo_query, limit: 1).first
+  end
+
+  def coords
+    [latitude, longitude]
+  end
+
+  def initialize_camera
+    location = Location.near(coords, 30).limit(1).first
+    self.camera = Camera.new(
+      latitude: latitude, 
+      longitude: longitude,
+      location: location,
+    )
+  end
+
+  def set_or_initialize_camera
+    set_camera
+    initialize_camera unless camera
+  end
+
   def delete_orphaned_camera
     camera.destroy if last_post
   end
 
   def last_post
-    camera.posts.size == 1
+    camera.posts.empty
   end
 end
